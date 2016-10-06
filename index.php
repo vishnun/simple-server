fcabf7dc517430f0fb6d85ed4ee3c1ff49205bf5
Vishnu Narang <vishnunarang@chilabs-imac-2.dhcp.asu.edu>
2016-10-06 10:13:13 -0700

    vishnu: First commit. Adding very basic stuff to show directory using php

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+ <head>
+ 	<link rel="stylesheet" type="text/css" href="./styles/main.css">
+ 	<script type="text/javascript" src="./javascripts/jquery-3.1.1.min.js"></script>
+  <title>Chi Lab</title>
+ </head>
+ <body>
+
+	<div class="header">
+	  <h1>Chi Lab Studies	</h1>
+	</div>
+	<div class="content">
+		<?
+	 		$dir_path = "./projects";
+			if ($handle = opendir($dir_path)) {
+			    while (false !== ($file = readdir($handle)))
+			    {
+			        if ($file != "." && $file != ".." && $file != ".DS_Store")
+			        {
+			            $thelist .= '<li class="item inline-b-item">
+			            	<div class="display-table">
+			            		<a href="'.$dir_path.'/'.$file.'">'.$file.' </a>
+			            	</div>
+			            </li>';
+			        }
+			    }
+			    closedir($handle);
+			}
+		?>
+
+
+		<ul class="container inline-b">
+		  <?=$thelist?>
+		</ul>
+
+	</div>
+ 	
+
+ 	<script type="text/javascript" src="./javascripts/header.js"></script>
+ </body>
+</html>
